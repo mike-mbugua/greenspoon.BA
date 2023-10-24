1b0089eb100ad5f3a0a3a80fc95aa4ec97826478
michael mbugua <mikembugua960@gmail.com>
2023-10-24 15:17:27 +0300

    create a reusable component with props

---
+++ src/components/events/EventCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./event.css";
+
+function EventCard({ description, image, name, people, type, confirmed }) {
+  return (
+    <div className="events">
+      <div className="event-card-container">
+        <img src={image} alt="" />
+        <h3 className="name">{name}</h3>
+        <h4>{type}</h4>
+        <p>{description}</p>
+        <p>
+          BAs required: <span className="name">{people}</span>
+        </p>
+
+        <span>status: {confirmed}</span>
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
